9b7b53385ce34d768a4737a0b4640f4bdab28ad7
Cristina De Rito <cristina.derito@outlook.com>
2016-08-18 11:06:51 +0200

    Add Layout

--- TextReader/ViewController.swift
+++ TextReader/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var cameraPreview: UIView!
+    @IBOutlet var recognizedText: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
